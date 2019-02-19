90850c5c0854eb079d298b303e81dec8d2a81663
DiegoGV95 <diego.garcia.viana@taiger.com>
2019-02-19 10:47:12 +0100

    "Introducción JS"

--- JavaScript/web/hola-mundo/js/27-ejercicio-arrays.js
+++ JavaScript/web/hola-mundo/js/27-ejercicio-arrays.js
@@ -12,55 +12,88 @@
 */
 
 var array = [];
-var aux = [];
-
 var elemento = "";
 
 for (var i = 0; i < 6; i++) {
 	elemento = parseInt(prompt("Elemento " + (i+1), ""));
-	array.push(elemento);
-	aux.push(elemento);
+	introducirElemento(elemento,array);
 }
 
-document.write("<h1>Los elementos introducidos son:</h1>");
-document.write("<ul>");
-
-for (let elemento of array) {
-	document.write("<li>" + elemento + "</li>");
-	console.log(elemento);
-}
-
-document.write("</ul>");
-
-document.write("<hr/>");
+document.write("<h2>Los elementos introducidos son:</h2>");
+mostrarArray(array);
 
 document.write("<h2>Los elementos ordenados son:</h2>");
-document.write("<ul>");
-
-for (let elemento of array.sort()) {
-	document.write("<li>" + elemento + "</li>");
-	console.log(elemento);
-}
-
-document.write("</ul>");
-
-document.write("<hr/>");
+var array_ordenado = ordenarArray(array);
+mostrarArray(array_ordenado);
 
 document.write("<h2>Los elementos invertidos son:</h2>");
-document.write("<ul>");
+var array_invertido = invertirArray(array);
+mostrarArray(array_invertido);
 
-for (let elemento of array.reverse()) {
-	document.write("<li>" + elemento + "</li>");
+getLongitud(array);
+
+var busqueda = parseInt(prompt("Introduce el numero que quieres buscar:", ""));
+getIndice(busqueda, array);
+
+function introducirElemento(elemento, array) {
+	array.push(elemento);
 	console.log(elemento);
 }
 
-document.write("</ul>");
+function mostrarArray(array) {
 
-document.write("<hr/>");
+	document.write("<ul>");
 
-document.write("<p><h3>El array tiene " + array.length + " elementos</p>");
+	for (let elemento of array) {
+		document.write("<li>" + elemento + "</li>");
+		console.log(elemento);
+	}
 
-var busqueda = parseInt(prompt("Introduce el numero que quieres buscar:", ""));
-var indice = aux.findIndex(indice => indice == busqueda);
+	document.write("</ul>");
 
-document.write("<p>El numero " + busqueda + " ocupa el indice " + indice + " dentro del array</p>");
+	document.write("<hr/>");
+
+}
+
+function ordenarArray(array) { // De menor a mayor
+	for (var i = 0; i < array.length; i++) {
+		for (var j = i+1; j < array.length; j++) {
+			if (array[i] > array[j]) {
+				var aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+			}
+		}
+	}
+	return array;
+}
+
+function invertirArray(array) { // De mayor a menor
+	for (var i = 0; i < array.length; i++) {
+		for (var j = i+1; j < array.length; j++) {
+			if (array[i] < array[j]) {
+				var aux = array[i];
+				array[i] = array[j];
+				array[j] = aux;
+			}
+		}
+	}
+	return array;
+}
+
+function getLongitud(array) {
+	document.write("<p><h3>El array tiene " + array.length + " elementos</p>");
+}
+
+function getIndice(index, array) {
+
+	var indice = array.findIndex(index => index == busqueda);
+
+	if (indice == -1)
+		alert("El numero indicado no se encuentra en el array introducido");
+	else
+		document.write("<p>El numero " + busqueda + " ocupa el indice " + indice + " dentro del array</p>");
+	
+}
+
+
