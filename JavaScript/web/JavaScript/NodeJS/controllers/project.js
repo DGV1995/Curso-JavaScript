74ed4280e7ea8af225c018e7eee6d9bc14d4c3f8
DiegoGV95 <diego.garcia.viana@taiger.com>
2019-02-28 17:03:16 +0100

    Controlador con NodeJS

--- JavaScript/web/JavaScript/NodeJS/controllers/project.js
+++ JavaScript/web/JavaScript/NodeJS/controllers/project.js
@@ -44,8 +44,119 @@ var controller = {
 		return response.status(200).send({
 			message: "Método saveProject",
 			project: project
+		});
+	},
+
+	// Obtener un documento de la colección
+	getProject: (request, response) => {
+
+		var projectId = request.params.id;
+
+		if (projectId == null)
+			return response.status(404).send({message: "El documento no existe"});
+
+		Project.findById(projectId, (error, project) => {
+
+			if (error) return response.status(500).send({message: "Error al devolver los datos"});
+
+			if (!project) return response.status(404).send({message: "El documento no existe"});
+
+			return response.status(200).send(project)
+
+		});
+
+	},
+
+	// Obtener todos los documentos de la colección
+	getProjects: (request, response) => {
+
+		//Project.find({langs:"Python"}).exec((error, projects) => { --> Se puede poner una condición (o varias) para la búsqueda
+
+		// Project.find({}).sort("-year".exec((error, projects) => { --> Ordeno por año, de menor a mayor (de mayot a menor --> "+year")
+
+		Project.find({}).exec((error, projects) => { // Sin condición saca todos los documentos
+
+
+			if (error) return response.status(500).send({message: "Error al devolver los datos"});
+
+			if (!projects) return response.status(404).send({message: "No hay documentos para mostrar"});
+
+			return response.status(200).send({projects});
+
+		});
+
+	},
+
+	// Actualizar un documento
+	updateProject: (request, response) => {
+
+		var projectId = request.params.id;
+		var update = request.body;
+
+		Project.findByIdAndUpdate(projectId, update, {new:true},(error, projectUpdated) => { // El tercer parámetro lo pasamos para que projectUpdated nos devuelva el documento actualizado en vez del antiguo
+
+			if (error) return response.status(500).send({message: "Error al actualizar los datos"});
+
+			if (!projectUpdated) return response.status(404).send({message: "No existe el documento para actualizar"});
+
+			return response.status(200).send(projectUpdated);
+
+		});
+
+	},
+
+	// Borrar un documento
+	deleteProject: (request, response) => {
+
+		var projectId = request.params.id;
+
+		//Project.findByIdAndRemove(projectId, (error, projectDeleted) => { (por si el de abajo no funciona)
+
+		Project.findByIdAndDelete(projectId, (error, projectDeleted) => {
+
+			if (error) return response.status(500).send({message: "Error al eliminar los datos"});
+
+			if (!projectDeleted) return response.status(404).send({message: "No existe el documento para eliminar"});
+
+			return response.status(200).send(projectDeleted);
+
 		})
-	} 
+
+	},
+
+	// Subir imágenes al servidor
+	uploadImage: (request, response) => {
+
+		var projectId = request.params.id;
+		var fileName = "Imagen no subida"; // Por defecto;
+
+		if (request.files) {
+
+			var filePath = request.files.image.path;
+			var fileSplit = filePath.split("/");
+			fileName = fileSplit[1];
+
+			Project.findByIdAndUpdate(projectId, {image: fileName}, {new: true}, (error, projectUpdated) => {
+
+				if (error) return response.status(500).send({message: "Error al subir la imagen"});
+
+				if (!projectUpdated) return response.status(404).send({message: "No hay documento para actualizar"});
+
+				return response.status(200).send(projectUpdated);
+
+			})
+
+		}
+		else
+			return response.status(200).send({message: fileName});
+
+	}
+
 };
 
 module.exports = controller;
+
+
+
+
+
--- JavaScript/web/JavaScript/NodeJS/routes/project.js
+++ JavaScript/web/JavaScript/NodeJS/routes/project.js
@@ -7,8 +7,20 @@ var ProjectController = require("../controllers/project");
 
 var router = express.Router();
 
+// Configuro el middleware para poder subir una imagen con el MultiParty
+// Un middleware es algo que se ejecuta antes de que se ejecute el método del controlador
+var multipart = require("connect-multiparty");
+var multipartMiddleware = multipart({uploadDir: "./uploads"});
+
 router.get("/home", ProjectController.home);
 router.post("/test", ProjectController.test);
 router.post("/save-project", ProjectController.saveProject);
+router.get("/get-project/:id?", ProjectController.getProject);
+// La '?' indica que 'id' es un parámentro OPCIONAL. 
+// Si el parámetro es obligatiorio, no se pone nada al lado
+router.get("/get-projects", ProjectController.getProjects);
+router.put("/update-project/:id", ProjectController.updateProject); // En este caso, el id va a ser obligatorio para saber qué documento actualizar
+router.delete("/delete-project/:id", ProjectController.deleteProject);
+router.post("/upload-image/:id", multipartMiddleware, ProjectController.uploadImage);
 
 module.exports = router;
