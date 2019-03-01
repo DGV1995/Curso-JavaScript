83559c2785ff5431b2de17d8adbfbf04b43bf795
DiegoGV95 <diego.garcia.viana@taiger.com>
2019-03-01 10:48:31 +0100

    Update app.js

--- JavaScript/web/JavaScript/NodeJS/app.js
+++ JavaScript/web/JavaScript/NodeJS/app.js
@@ -17,13 +17,13 @@ app.use(bodyParser.json()); // Cualquier tipo de petición que me llegue, lo con
 
 // CORS Y CABECERAS
 // Configuramos un middleware para tener el menor número de problemas con el back-end --> Se va a ejecutar siempre antes de realizar cada petición
-app.use(request, response, next) => {
+app.use((request, response, next) => {
 	request.header("Access-Control-Allow-Origin", "*");
 	request.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Conten-Type, Accept, Access-Control-Allow-Request-Method");
 	resquest.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
 	request.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
 	next();
-}
+});
 
 // Cargar rutas
 app.use("/api", project_routes);
